6549a66d9dc8913226a6f0da736337c8fbda82c6
Sabri-Simo <https://github.com/Sabri-Simo/Crud.git>
2024-09-01 20:50:53 +0200

    crud

--- db/connection.go
+++ db/connection.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,8 +12,18 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "/**/"
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
+	// Get the DATABASE_URL from environment variables
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	// Connect to the database
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
